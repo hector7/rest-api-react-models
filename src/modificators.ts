9355e86a3dac5a60ee1216145d6ba1d266c7f06b
Héctor <hector.rbs91@gmail.com>
2020-01-29 17:44:59 +0100

    added post, put, delete

---
+++ src/modificators.ts
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Redux as ReducerNamespace } from '@rest-api/redux/src/ReducerStorage'
+import { Redux as SchemaNamespace } from '@rest-api/redux/src/Schema'
+import { Model, HttpError, Callback } from '@rest-api/redux'
+import { InferableComponentEnhancerWithProps, shallowEqual, GetProps } from 'react-redux'
+import { useDispatch, useSelector } from '..'
+
+export namespace Modificators {
+    export type PromsFromItem<Item> = {
+        post: (item: Partial<Item>, callback: Callback<Item, HttpError>) => any,
+        put: (id: Item[any], item: Partial<Item>, callback: Callback<Item, HttpError>) => any,
+        remove: (item: Item, callback: Callback<undefined, HttpError>) => any
+    }
+}
+
+
+
+export function useModificators<ItemType extends SchemaNamespace.Item, Item extends SchemaNamespace.RealType<ItemType>, Metadata>(
+    model: Model<ItemType, Item, any, any, any, {}, Metadata>,
+): Modificators.PromsFromItem<Item> {
+    const { post, put, delete: remove } = model.actions
+    const dispatch = useDispatch()
+    return <{
+        post: typeof post,
+        put: typeof put,
+        remove: typeof remove
+    }>{
+            post: (...args) => dispatch(post(...args)),
+            put: (...args) => dispatch(put(...args)),
+            remove: (...args) => dispatch(remove(...args)),
+        }
+}
+
+
+export default function connectModificators<ItemType extends SchemaNamespace.Item, Item extends SchemaNamespace.RealType<ItemType>, Metadata>(
+    model: Model<ItemType, Item, any, any, any, {}, Metadata>,
+): InferableComponentEnhancerWithProps<Modificators.PromsFromItem<Item>, {}> {
+    return (ReactComponent): any => {
+        const ObjectRaising: React.FunctionComponent<GetProps<typeof ReactComponent>> = (props) => {
+            const result = useModificators(model)
+            return React.createElement(ReactComponent, { ...props, ...result })
+        }
+        return ObjectRaising
+    }
+}
