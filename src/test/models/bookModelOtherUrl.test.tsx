2a59cb8c2b58b7843c517215a8cab424f5fe91f8
Héctor <hector.rbs91@gmail.com>
2020-06-10 10:34:12 +0200

    added describe funcs

--- src/test/models/bookModelOtherUrl.test.tsx
+++ src/test/models/bookModelOtherUrl.test.tsx
@@ -5,62 +5,63 @@ import { render } from '@testing-library/react';
 import { getProvider } from '../../..'
 declare var global: any
 import FakeXMLHttpRequest from "fake-xml-http-request";
-
-test('useGet works', (d) => {
-    function LibraryComponentGet() {
-        const result = bookModelOtherUrl.useGet()
-        return <React.Fragment>
-            {result.items.map(i => <p key={i.id}>{i.name}</p>)}
-        </React.Fragment>
-    }
-    global.XMLHttpRequest = class XMLHttpRequest extends FakeXMLHttpRequest {
-        send() {
-            switch (this.responseURL) {
-                case '/api/otherUrl':
-                    setTimeout(() => {
-                        this.respond(200, {}, JSON.stringify([{ id: 1, name: 'pepita', library: 1 }] as BookType[]))
-                    }, 40)
+describe('getSearchSubmodel', () => {
+    test('useGet works', (d) => {
+        function LibraryComponentGet() {
+            const result = bookModelOtherUrl.useGet()
+            return <React.Fragment>
+                {result.items.map(i => <p key={i.id}>{i.name}</p>)}
+            </React.Fragment>
+        }
+        global.XMLHttpRequest = class XMLHttpRequest extends FakeXMLHttpRequest {
+            send() {
+                switch (this.responseURL) {
+                    case '/api/otherUrl':
+                        setTimeout(() => {
+                            this.respond(200, {}, JSON.stringify([{ id: 1, name: 'pepita', library: 1 }] as BookType[]))
+                        }, 40)
+                }
             }
         }
-    }
-    const Provider = getProvider()
-    const { getByText } = render(<Provider>
-        <LibraryComponentGet />
-    </Provider>);
-    setTimeout(() => {
-        const linkElement = getByText(/pepita/i);
-        d()
-    }, 60)
-});
+        const Provider = getProvider()
+        const { getByText } = render(<Provider>
+            <LibraryComponentGet />
+        </Provider>);
+        setTimeout(() => {
+            const linkElement = getByText(/pepita/i);
+            d()
+        }, 60)
+    });
 
-test('useGetPopulated works', (d) => {
-    function LibraryComponentGetPopulated() {
-        const result = bookModelOtherUrl.useGetPopulated()
-        return <React.Fragment>
-            {result.populated && result.items.map(i => <p key={i.id}>{i.library.name}</p>)}
-            {!result.populated && result.items.map(i => <p key={i.id}>{i.library.name}</p>)}
-        </React.Fragment>
-    }
-    global.XMLHttpRequest = class XMLHttpRequest extends FakeXMLHttpRequest {
-        send() {
-            switch (this.responseURL) {
-                case '/api/otherUrl':
-                    return setTimeout(() => {
-                        this.respond(200, {}, JSON.stringify([{ id: 1, name: 'book', library: 1 }] as BookType[]))
-                    }, 10)
-                case '/api/library/1/':
-                    return setTimeout(() => {
-                        this.respond(200, {}, JSON.stringify({ id: 1, name: 'book' } as BookType))
-                    }, 10)
+    test('useGetPopulated works', (d) => {
+        function LibraryComponentGetPopulated() {
+            const result = bookModelOtherUrl.useGetPopulated()
+            return <React.Fragment>
+                {result.populated && result.items.map(i => <p key={i.id}>{i.library.name}</p>)}
+                {!result.populated && result.items.map(i => <p key={i.id}>{i.library.name}</p>)}
+            </React.Fragment>
+        }
+        global.XMLHttpRequest = class XMLHttpRequest extends FakeXMLHttpRequest {
+            send() {
+                switch (this.responseURL) {
+                    case '/api/otherUrl':
+                        return setTimeout(() => {
+                            this.respond(200, {}, JSON.stringify([{ id: 1, name: 'book', library: 1 }] as BookType[]))
+                        }, 10)
+                    case '/api/library/1/':
+                        return setTimeout(() => {
+                            this.respond(200, {}, JSON.stringify({ id: 1, name: 'book' } as BookType))
+                        }, 10)
+                }
             }
         }
-    }
-    const Provider = getProvider()
-    const { getByText, container } = render(<Provider>
-        <LibraryComponentGetPopulated />
-    </Provider>);
-    setTimeout(() => {
-        const linkElement = getByText(/book/i);
-        d()
-    }, 60)
-});
+        const Provider = getProvider()
+        const { getByText, container } = render(<Provider>
+            <LibraryComponentGetPopulated />
+        </Provider>);
+        setTimeout(() => {
+            const linkElement = getByText(/book/i);
+            d()
+        }, 60)
+    });
+})
--- src/test/models/libraryByNameExtended.test.tsx
+++ src/test/models/libraryByNameExtended.test.tsx
@@ -6,57 +6,59 @@ import { getProvider } from '../../..'
 declare var global: any
 import FakeXMLHttpRequest from "fake-xml-http-request";
 
-test('useGetById works', (d) => {
-    function LibraryComponentGetById() {
-        const result = libraryByNameExtended.useGetById({ project: 'real' }, 'pepita')
-        return <React.Fragment>
-            <p>{result.item?.name}</p>
-        </React.Fragment>
-    }
-    global.XMLHttpRequest = class XMLHttpRequest extends FakeXMLHttpRequest {
-        send() {
-            switch (this.responseURL) {
-                case '/api/libraryByName/real/pepita/':
-                    setTimeout(() => {
-                        this.respond(200, {}, JSON.stringify({ id: 1, name: 'pepita' } as LibraryType))
-                    }, 40)
+describe('getSubModelWithKey extended', () => {
+    test('useGetById works', (d) => {
+        function LibraryComponentGetById() {
+            const result = libraryByNameExtended.useGetById({ project: 'real' }, 'pepita')
+            return <React.Fragment>
+                <p>{result.item?.name}</p>
+            </React.Fragment>
+        }
+        global.XMLHttpRequest = class XMLHttpRequest extends FakeXMLHttpRequest {
+            send() {
+                switch (this.responseURL) {
+                    case '/api/libraryByName/real/pepita/':
+                        setTimeout(() => {
+                            this.respond(200, {}, JSON.stringify({ id: 1, name: 'pepita' } as LibraryType))
+                        }, 40)
+                }
             }
         }
-    }
-    const Provider = getProvider()
-    const { getByText } = render(<Provider>
-        <LibraryComponentGetById />
-    </Provider>);
-    setTimeout(() => {
-        const linkElement = getByText(/pepita/i);
-        d()
-    }, 60)
-});
+        const Provider = getProvider()
+        const { getByText } = render(<Provider>
+            <LibraryComponentGetById />
+        </Provider>);
+        setTimeout(() => {
+            const linkElement = getByText(/pepita/i);
+            d()
+        }, 60)
+    });
 
-test('useGetPopulatedById works', (d) => {
-    function LibraryComponentGetByIdPopulated() {
-        const result = libraryByNameExtended.useGetByIdPopulated({ project: 'real' }, 'pepita')
-        return <React.Fragment>
-            <p>{result.item?.name}</p>
-        </React.Fragment>
-    }
-    global.XMLHttpRequest = class XMLHttpRequest extends FakeXMLHttpRequest {
-        send() {
-            switch (this.responseURL) {
-                case '/api/libraryByName/real/pepita/':
-                    return setTimeout(() => {
-                        this.respond(200, {}, JSON.stringify({ id: 1, name: 'book' } as LibraryType))
-                    }, 10)
+    test('useGetPopulatedById works', (d) => {
+        function LibraryComponentGetByIdPopulated() {
+            const result = libraryByNameExtended.useGetByIdPopulated({ project: 'real' }, 'pepita')
+            return <React.Fragment>
+                <p>{result.item?.name}</p>
+            </React.Fragment>
+        }
+        global.XMLHttpRequest = class XMLHttpRequest extends FakeXMLHttpRequest {
+            send() {
+                switch (this.responseURL) {
+                    case '/api/libraryByName/real/pepita/':
+                        return setTimeout(() => {
+                            this.respond(200, {}, JSON.stringify({ id: 1, name: 'book' } as LibraryType))
+                        }, 10)
+                }
             }
         }
-    }
-    const Provider = getProvider()
-    const { getByText } = render(<Provider>
-        <LibraryComponentGetByIdPopulated />
-    </Provider>);
-    d('not works as maximum update ... all is called but raises an error.')
-    setTimeout(() => {
-        const linkElement = getByText(/book/i);
-        d()
-    }, 60)
-});
+        const Provider = getProvider()
+        const { getByText } = render(<Provider>
+            <LibraryComponentGetByIdPopulated />
+        </Provider>);
+        d('not works as maximum update ... all is called but raises an error.')
+        setTimeout(() => {
+            const linkElement = getByText(/book/i);
+            d()
+        }, 60)
+    });
+})
